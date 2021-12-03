3baa458b41dc2da58bdbaf71cf255a79ff9a9e9e
Umut Özdemir <umutozdemir997@gmail.com>
2021-12-03 21:10:06 +0300

    improve types

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,68 @@
 import { DataProvider } from "@pankod/refine";
 
-const hasKey = <T extends object>(obj: T, k: keyof any): k is keyof T => k in obj;
+const customizableDataProviderKeys = [
+  "getList",
+  "getMany",
+  "getOne",
+  "create",
+  "createMany",
+  "update",
+  "updateMany",
+  "deleteOne",
+  "deleteMany",
+] as const;
 
 type DataProviderKey = keyof DataProvider;
+type CustomizableDataProviderKey = typeof customizableDataProviderKeys[number];
 type DataProviderMethod<T extends DataProviderKey> = Required<DataProvider>[T];
 type DataProviderMethodParameters<T extends DataProviderKey> = Parameters<
+  DataProviderMethod<T>
+>;
+type DataProviderCustomizableMethodParameters<T extends CustomizableDataProviderKey> = Parameters<
   DataProviderMethod<T>
 >;
 
 type CustomizationsType = Record<string, Partial<Omit<DataProvider, "custom">>>;
 
+function keyIsCustomizable(key: any): key is CustomizableDataProviderKey {
+  return customizableDataProviderKeys.includes(key);
+}
+
+const hasKey = <T extends object>(obj: T, k: keyof any): k is keyof T =>
+  k in obj;
+
 export function customize(
   source: DataProvider,
-  // customizations?: CustomizationsType,
+  customizations?: CustomizationsType,
 ): DataProvider {
-  // const customizedMethods: Record<string, CustomizationsType> = {};
-
   const proxiedSource = new Proxy(source, {
     get: function proxyGetHandler<
-      T extends DataProviderKey,
-      A extends DataProviderMethodParameters<T>,
-      R extends ReturnType<DataProviderMethod<T>>,
+      K extends DataProviderKey,
+      R extends ReturnType<DataProviderMethod<K>>,
     >(
       target: DataProvider,
-      key: T,
+      key: K,
     ) {
-      if(key === "custom") {
+      if (!keyIsCustomizable(key)) {
         return target[key];
       }
 
-      return function customizerHandler(
-        ...params: A
+      return function customizerHandler<
+      M extends DataProviderCustomizableMethodParameters<typeof key>>(
+        ...params: M
       ): R {
+        const firstParam = params?.[0];
+
+        if(!firstParam) {
+          throw new Error("Please pass arguments to data provider method");
+        }
+
+        const resource = firstParam.resource;
+
+        if (typeof customizations?.[resource] !== "undefined") {
+
+        }
+
         const methodOnBase = target[key];
 
         if (
